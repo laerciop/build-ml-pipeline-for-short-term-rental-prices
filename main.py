1f8c1d41c9628e565075e94eaa88ff1ed001fcac
Laercio Pires Neto <49697468+laerciop@users.noreply.github.com>
2022-09-03 17:25:34 +0100

    wtd

--- main.py
+++ main.py
@@ -82,9 +82,9 @@ def go(config: DictConfig):
             _ = mlflow.run(f"{config['main']['components_repository']}/train_val_test_split",
                             "main",
                             parameters={"input": "clean_sample.csv:latest",
-                                        "test_size": config["modelling"]["test_size"],
-                                        "random_seed": config["modelling"]["random_seed"],
-                                        "stratify_by": config["modelling"]["stratify_by"]
+                                        "test_size": config["modeling"]["test_size"],
+                                        "random_seed": config["modeling"]["random_seed"],
+                                        "stratify_by": config["modeling"]["stratify_by"]
                                         }
                             )
 
